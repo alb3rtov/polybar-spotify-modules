e34e4a0afb12cd56547b410eb132e1a0736335a2
Alberto <40604222+alb3rtov@users.noreply.github.com>
2022-10-30 18:26:13 +0100

    Update check-sfy-running.sh

--- check-sfy-running.sh
+++ check-sfy-running.sh
@@ -3,7 +3,7 @@
 if [ $(pgrep spotify | wc -l) -gt 0 ]
 then
   song=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata | sed -n '/title/{n;p}' | cut -d '"' -f 2 2> /dev/null)
-	band=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata | grep -A2 "artist" | tail -1 | awk '{print $2}' | tr -d '"' 2> /dev/null)
+  band=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata | grep -A2 "artist" | tail -1 | awk '{print $2}' | tr -d '"' 2> /dev/null)
   polybar-msg action '#spotifycontrols.open.0' &> /dev/null
   
   if [ $band ]
@@ -14,6 +14,6 @@ then
   fi
 
 else
-	echo "%{F#66ffffff}"
+  echo "%{F#66ffffff}"
   polybar-msg action '#spotifycontrols.close.0' &> /dev/null
 fi
