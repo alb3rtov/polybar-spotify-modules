31a4918cfc3f95b2422c34917dd8b3de91bd492f
Alberto <40604222+alb3rtov@users.noreply.github.com>
2022-10-31 13:47:07 +0100

    Update check-sfy-running.sh

--- check-sfy-running.sh
+++ check-sfy-running.sh
@@ -3,10 +3,10 @@
 if [ $(pgrep spotify | wc -l) -gt 0 ]
 then
   song=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata | sed -n '/title/{n;p}' | cut -d '"' -f 2 2> /dev/null)
-  band=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata | grep -A2 "artist" | tail -1 | awk '{print $2}' | tr -d '"' 2> /dev/null)
+  band=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata | sed -n '/artist/{n;n;p}' | cut -d '"' -f 2 2> /dev/null)
   polybar-msg action '#spotifycontrols.open.0' &> /dev/null
   
-  if [ $band ]
+  if [ ${#band} -gt 0 ]
   then
     echo "%{F#9feb6a} %{F#696969} $band - $song" 
   else
